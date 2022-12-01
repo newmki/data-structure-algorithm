8d9365ccaff891bd44cf6a4c2f7cd8bf02117d59
owen.ki <owen.ki.dev@gmail.com>
2022-12-01 15:45:20 +0900

    Add dijkstra

--- graph/dijkstra.js
+++ graph/dijkstra.js
@@ -6,12 +6,12 @@
 
 const INF = Infinity;
 const arr = [
-  [0, 2, 5, 1, INF, INF],
-  [2, 0, 3, 2, INF, INF],
-  [5, 3, 0, 3, 1, 5],
-  [1, 2, 3, 0, 1, INF],
-  [INF, INF, 1, 1, 0, 2],
-  [INF, INF, 5, INF, 2, 0],
+  [0, 10, 1, 4, INF, INF],
+  [10, 0, 2, 5, INF, INF],
+  [1, 2, 0, 1, INF, 8],
+  [4, 5, 1, 0, INF, 3],
+  [INF, INF, INF, INF, 0, 2],
+  [INF, INF, 8, 3, 2, 0],
 ];
 const isVisit = new Array(6).fill(false);
 
@@ -40,7 +40,9 @@ const dist = function (start) {
     min += startV[idx];
     const next = arr[idx];
     for (let i = 0; i < v.length; i++) {
-      if (v[i] > next[i] + min && !isVisit[i]) v[i] = next[i] + min;
+      if (v[i] > next[i] + min && !isVisit[i]) {
+        v[i] = next[i] + min;
+      }
     }
     startV = arr[idx];
     isVisit[idx] = true;
